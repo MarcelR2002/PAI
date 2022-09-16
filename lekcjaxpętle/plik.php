4c83cd4dca34a1b2f7a4f6e617395a5a4c40fb5f
Marcel Ruszkowski (Uczeń) <mruszkowski183@technikumkreatywne.pl>
2022-09-16 15:40:33 +0200

    Opanowałem znajomość pętli for

---
+++ lekcjaxpętle/plik.php
@@ -0,0 +1,33 @@
+<?php
+     
+	 for($count1 = 1; $count1 <=6; ++$count1) {
+	     echo "<br>";
+	 for($count = 1; $count <=10; ++$count)  {
+		 echo "*";
+		
+		 }
+	     }
+	 
+	
+	
+	
+	
+	
+	
+	
+	echo "<hr>";
+	for($count = 1; $count <= 10; ++$count){
+		echo "<br>";
+		for ($count1 = 1; $count1 <=10; ++$count1)
+		{
+		echo($count."*".$count1."=".($count*$count1)."<br>");
+		}
+	    }
+
+		
+    
+
+
+
+?>
+
